6a121a868bfef0f126852592b9731e5400f309d8
Mithun Yadav <itsmithun01@gmail.com>
2024-11-10 21:10:49 +0530

    Fixed Error

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ async def download_poster(poster_url):
 def format_caption(movie, year, audio, genre, synopsis):
     """Format the caption with Markdown"""
     caption = f""" {movie} （{year}）
-» 𝗔𝘂𝗱𝗶𝗼: {audio}
+» 𝗔𝘂𝗱𝗶𝗼: {audio}（Esub） 
 » 𝗤𝘂𝗮𝗹𝗶𝘁𝘆: 480p | 720p | 1080p 
 » 𝗚𝗲𝗻𝗿𝗲: {genre}
 » 𝗦𝘆𝗻𝗼𝗽𝘀𝗶𝘀
