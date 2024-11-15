4943c9034cf380dec744d5b28a4dcfe3f27d8bea
mithun-ctrl <itsmithun01@gmail.com>
2024-11-15 20:43:44 +0530

    foramt design

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ async def caption_command(client, message):
 
         # Add additional message if -f or -filename is present
         if include_filename:
-            additional_message = f"`[PirecyKings2] {movie_data['movie_p']} ({movie_data['year_p']}) @pirecykings2.mkv`"
+            additional_message = f"`[PirecyKings2] {movie_data['movie_p']} ({movie_data['year_p']}) @pirecykings2`"
             await client.send_message(
                 chat_id=message.chat.id,
                 text=additional_message,
@@ -285,7 +285,7 @@ async def caption_command(client, message):
 
         # Add additional message if -db or -database is present
         if include_database:
-            additional_message = f"""`[PirecyKings2] {movie_data['movie_p']} ({movie_data['year_p']}) @pirecykings2.mkv`
+            additional_message = f"""`[PirecyKings2] {movie_data['movie_p']} ({movie_data['year_p']}) @pirecykings2`
             
             `{movie_data['movie_p']} ({movie_data['year_p']}) 480p - 1080p [{movie_data['audio_p']}]`"""
             await client.send_message(
